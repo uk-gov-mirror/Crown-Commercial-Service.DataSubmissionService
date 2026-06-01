08c45def3f1f4668451d78cdedda809d232ff1f3
Reem Ibrahim <reem.ibrahim@crowncommercial.gov.uk>
2026-06-01 13:38:53 +0100

    updated omniauth config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,15 +3,18 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
 
+  auth0_domain = ENV.fetch('AUTH0_DOMAIN')
+  auth0_user_info_domain = ENV.fetch('AUTH0_USER_INFO_DOMAIN', auth0_domain)
+
   provider(
     :auth0,
     ENV['AUTH0_CLIENT_ID'],
     ENV['AUTH0_CLIENT_SECRET'],
-    ENV['AUTH0_DOMAIN'],
+    auth0_domain,
     callback_path: '/auth/auth0/callback',
     authorize_params: {
       scope: 'openid profile',
-      audience: "https://#{ENV['AUTH0_DOMAIN']}/userinfo"
+      audience: "https://#{auth0_user_info_domain}/userinfo"
     }
   )
 end
